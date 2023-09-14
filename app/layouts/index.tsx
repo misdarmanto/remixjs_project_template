9e5f66df02fbbbd1f50633cd6e8135711e123140
misdarmanto <misdarmanto1922@gmail.com>
2023-09-14 21:08:54 +0700

    fix user name

--- app/layouts/index.tsx
+++ app/layouts/index.tsx
@@ -218,7 +218,7 @@ export default function Layout({
                 </Menu.Items>
               </Transition>
             </Menu>
-            <label htmlFor="" className="text-gray-500 font-md">
+            <label htmlFor="" className="hidden md:block text-gray-500 font-md">
               {session?.adminName}
             </label>
           </div>
